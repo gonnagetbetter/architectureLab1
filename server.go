f5709f1840545f3da83d782bc837551a329deba9
den <denissytnikmacho@gmail.com>
2023-02-26 21:13:51 +0200

    added status

--- server.go
+++ server.go
@@ -10,11 +10,13 @@ import (
 	CurrentTime string `json:"current_time"`
   }
 
-  func main() {
+func main() {
 	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet {
 		  http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		  return
-		}
-
+	}
+	
+	status := "we are clowns"
 }
+
