dac2c217374e401962f5a00426bbb61e3b1bcca9
den <denissytnikmacho@gmail.com>
2023-02-26 21:14:30 +0200

    Revert "added status"
    This reverts commit f5709f1840545f3da83d782bc837551a329deba9.

--- server.go
+++ server.go
@@ -10,13 +10,11 @@ import (
 	CurrentTime string `json:"current_time"`
   }
 
-func main() {
+  func main() {
 	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet {
 		  http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		  return
-	}
-	
-	status := "we are clowns"
-}
+		}
 
+}
