c19a1d893c64e5316ca5e4dea785daecde2e6389
den <denissytnikmacho@gmail.com>
2023-02-26 23:50:11 +0200

    finalised project

--- server.go
+++ server.go
@@ -7,29 +7,35 @@ import (
 )
 
 type TimeResponse struct {
-	CurrentTime string `json:"current_time"`
+  CurrentTime string `json:"current_time"`
 }
 
 func main() {
-	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-    
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		})
+  http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodGet {
+      http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+      return
+    }
 
-		currentTime := time.Now().Format(time.RFC3339)
-		response := TimeResponse{currentTime}
+    currentTime := time.Now().Format(time.RFC3339)
+    response := TimeResponse{currentTime}
 
-		jsonBytes, err := json.Marshal(response)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+    jsonBytes, err := json.Marshal(response)
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
 
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(jsonBytes)
-	})
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(jsonBytes)
+  })
+
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    http.Error(w, "Not found", http.StatusNotFound)
+  })
+
+  err := http.ListenAndServe(":8080", nil)
+  if err != nil {
+    panic(err)
+  }
 }
