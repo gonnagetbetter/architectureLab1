aefc821ab532f8476d4894e400524453ef738a44
gonnagetbetter <denissss906@gmail.com>
2023-02-26 21:02:23 +0200

    Added basic structure and handleFunc

--- server.go
+++ server.go
@@ -1,1 +1,20 @@
-Place code here
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+  )
+  
+  type TimeResponse struct {
+	CurrentTime string `json:"current_time"`
+  }
+
+  func main() {
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+		  http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		  return
+		}
+
+}
